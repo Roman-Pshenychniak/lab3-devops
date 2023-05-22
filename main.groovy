c3bf24064517992725d20f9c7d7f3736c76f37ed
RomanMac <romanmac@RomanMacs-MacBook-Air.local>
2023-05-23 01:51:00 +0300

    first commit

---
+++ main.groovy
@@ -0,0 +1,29 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git 'https://github.com/Roman-Pshenychniak/lab1-devops'
+            }
+        }
+
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    docker.build("my-image")
+                }
+            }
+        }
+
+        stage('Push Docker Image') {
+            steps {
+                script {
+                    withDockerRegistry([credentialsId: 'romanpshenychniak', url: 'https://your-docker-registry']) {
+                        dockerImage.push()
+                    }
+                }
+            }
+        }
+    }
+}
